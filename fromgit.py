0f80f5285d4808d6ea41351140794da0f0c730f1
deepesh1612 <deepeshsahu1612@gmacil.com>
2025-07-26 21:30:53 +0530

    new line has been added

--- fromgit.py
+++ fromgit.py
@@ -6,5 +6,9 @@ fromgit = Flask(__name__)
 def helloworld():
     return 'hello world from git'
 
+@fromgit.route('/',methods=['POST'])
+def helloworld():
+    return 'About Us'
+
 if __name__=="__main__":
     fromgit.run(port=3000,debug=True)
